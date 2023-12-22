49b309c36fae85287c4a0fa0a26cf566d9896391
Chuangxin Lin <lcxustc@gmail.com>
2023-12-22 12:22:48 -0500

    Ch02. Added validations.

--- ch02/taco-cloud/src/main/java/sia/tacocloud/web/DesignTacoController.java
+++ ch02/taco-cloud/src/main/java/sia/tacocloud/web/DesignTacoController.java
@@ -1,8 +1,10 @@
 package sia.tacocloud.web;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import sia.tacocloud.Ingredient;
@@ -65,10 +67,17 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(Taco taco,
+    public String processTaco(@Valid Taco taco,
+                              Errors errors,
                               @ModelAttribute TacoOrder tacoOrder) {
+
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}", taco);
+
         return "redirect:/orders/current";
     }
 
