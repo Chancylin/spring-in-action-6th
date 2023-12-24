66a204d45ec0f6bc149b5ec5293e8f55ebc639cc
Chuangxin Lin <lcxustc@gmail.com>
2023-12-23 19:56:16 -0500

    Ch03-jpa. Now used Spring Data JPA.

--- ch03/taco-cloud-sd-jpa/src/main/java/sia/tacocloud/Ingredient.java
+++ ch03/taco-cloud-sd-jpa/src/main/java/sia/tacocloud/Ingredient.java
@@ -1,19 +1,18 @@
 package sia.tacocloud;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.domain.Persistable;
-import org.springframework.data.relational.core.mapping.Table;
 
 @Data
-@Table
+@Entity
 @AllArgsConstructor
-@NoArgsConstructor(access= AccessLevel.PRIVATE, force=true)
-public class Ingredient
-        implements Persistable<String> {
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force=true)
+public class Ingredient {
 
     // clin: because now we create Ingredient objects via CommandLineRunner()
     // in TacoCloudApplication(), now these attributes cannot be "final"
@@ -22,12 +21,6 @@ public class Ingredient
 
     private String name;
     private Type type;
-
-    @Override
-    public boolean isNew() {
-        // clin: what is the purpose of this?
-        return true;
-    }
 
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
