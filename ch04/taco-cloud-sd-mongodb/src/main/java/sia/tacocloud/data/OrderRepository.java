53b29c0773f1c28f0fb1d9c629cbd3cbc0e7fad4
Chuangxin Lin <lcxustc@gmail.com>
2023-12-31 23:09:51 -0500

    Ch04-mongodb. Followed the book, but needed to use the new de.flapdoodle.embed.mongo.spring3x. However, cannot start webapp, because 'No server chosen by WritableServerSelector from cluster description...'

--- ch04/taco-cloud-sd-mongodb/src/main/java/sia/tacocloud/data/OrderRepository.java
+++ ch04/taco-cloud-sd-mongodb/src/main/java/sia/tacocloud/data/OrderRepository.java
@@ -3,9 +3,7 @@ package sia.tacocloud.data;
 import org.springframework.data.repository.CrudRepository;
 import sia.tacocloud.TacoOrder;
 
-import java.util.UUID;
-
 public interface OrderRepository
-        extends CrudRepository<TacoOrder, UUID> {
+        extends CrudRepository<TacoOrder, String> {
 
 }
